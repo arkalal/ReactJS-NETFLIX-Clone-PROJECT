f5aee5287d533e6e100b0a9e00b112d345d1e433
arkalal <arkalal.123@gmail.com>
2022-02-07 23:05:47 +0530

    user authentication  functionalities complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
+import { auth } from './firebase/firebase';
 import Home from './home/Home';
 import Login from './login/Login';
+import { login, logout } from './reduxState/userSlice';
 
 const App = () => {
 
     const user = useSelector((state) => state.user.user)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        auth.onAuthStateChanged((userAuth) => {
+            if (userAuth) {
+                dispatch(login({
+                    uid: userAuth.uid,
+                    email: userAuth.email
+                }))
+            } else {
+                dispatch(logout())
+            }
+        })
+    }, [dispatch])
 
     return <div>
         {
--- src/signup/Signup.js
+++ src/signup/Signup.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Signup.css'
+import { auth } from '../firebase/firebase'
+import { useDispatch } from 'react-redux';
+import { login } from '../reduxState/userSlice'
 
 const Signup = () => {
+
+    const emailRef = useRef(null)
+    const passwordRef = useRef(null)
+
+    const dispatch = useDispatch()
+
+    const signMeUp = (e) => {
+        e.preventDefault()
+
+        auth.signInWithEmailAndPassword(emailRef.current.value, passwordRef.current.value).then((userAuth) => {
+            dispatch(login({
+                uid: userAuth.user.uid,
+                email: userAuth.user.email
+            }))
+        }).catch((error) => {
+            alert(error)
+        })
+    }
+
+    const register = (e) => {
+        e.preventDefault()
+        auth.createUserWithEmailAndPassword(emailRef.current.value, passwordRef.current.value).then((userAuth) => {
+            dispatch(login({
+                uid: userAuth.user.uid,
+                email: userAuth.user.email
+            }))
+        }).catch((error) => {
+            alert(error)
+        })
+    }
+
     return <div className='signup'>
         <form action="">
             <h1>Sign In</h1>
-            <input type="email" name="" id="" placeholder='Email Address' />
-            <input type="password" name="" id="" placeholder='Password' />
-            <button>Sign In</button>
+            <input type="email" ref={emailRef} name="" id="" placeholder='Email Address' />
+            <input type="password" ref={passwordRef} name="" id="" placeholder='Password' />
+            <button onClick={signMeUp}>Sign In</button>
 
-            <h4>New to Netflix? <span>Sign Up Now.</span></h4>
+            <h4>New to Netflix? <span onClick={register}>Sign Up Now.</span></h4>
         </form>
     </div>;
 };
