900de4119a559421a093cff4cfdadbd60bfdeb19
arkalal <arkalal.123@gmail.com>
2022-02-07 16:50:05 +0530

    Styled the login and signup page

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Signup from '../signup/Signup';
 import './Login.css'
 
 const Login = () => {
+
+    const [Signin, setSignin] = useState(false);
+
+    const goSignIn = (e) => {
+        e.preventDefault()
+        setSignin(true)
+    }
+
     return <div className='login'>
         <div className="loginHeader">
             <img src="https://www.teahub.io/photos/full/21-212834_netflix-logo-png-image-in-high-definition-netflix.png" alt="" />
@@ -10,15 +19,24 @@ const Login = () => {
         </div>
 
         <div className="loginBody">
-            <h1>Unlimited movies, TV shows and more.</h1>
-            <h2>Watch anywhere. Cancel anytime.</h2>
-            <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+            {
+                Signin ?
+                    (<Signup></Signup>)
+                    :
+                    (<>
+                        <h1>Unlimited movies, TV shows and more.</h1>
+                        <h2>Watch anywhere. Cancel anytime.</h2>
+                        <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
 
-            <form action="" className="loginForm">
-                <input type="text" placeholder='Email Address' />
-                <button>Get Started</button>
-            </form>
-        </div>
+                        <form action="" className="loginForm">
+                            <input type="email" placeholder='Email Address' />
+                            <button onClick={goSignIn}>Get Started</button>
+                        </form>
+                    </>)
+            }
+        </div >
+
+        <div className="loginGradient"></div>
     </div>;
 };
 
---
+++ src/signup/Signup.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Signup.css'
+
+const Signup = () => {
+    return <div className='signup'>
+        <form action="">
+            <h1>Sign In</h1>
+            <input type="email" name="" id="" placeholder='Email Address' />
+            <input type="password" name="" id="" placeholder='Password' />
+            <button>Sign In</button>
+
+            <h4>New to Netflix? <span>Sign Up Now.</span></h4>
+        </form>
+    </div>;
+};
+
+export default Signup;
