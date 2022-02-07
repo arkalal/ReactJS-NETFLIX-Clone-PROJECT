deccb50e37e455b0c6bde7fb5b10a4c2eca80580
arkalal <arkalal.123@gmail.com>
2022-02-07 21:51:24 +0530

    Setup firebase and redux and working on userlogin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Home from './home/Home';
 import Login from './login/Login';
 
 const App = () => {
+
+    const user = useSelector((state) => state.user.user)
+
     return <div>
-        <Login></Login>
+        {
+            user ?
+                <Home></Home>
+                :
+                <Login></Login>
+        }
     </div>;
 };
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import reactDom from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import './index.css'
+import store from './reduxState/store'
 
 reactDom.render(<BrowserRouter>
-    <App></App>
+    <Provider store={store}>
+        <App></App>
+    </Provider>
 </BrowserRouter>, document.getElementById('root'))
